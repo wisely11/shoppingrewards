63ad2b8a7bca26dd5af5e7c81385672f85fea9a7
Wisely <wiselys@kriyatec.com>
2025-08-31 15:39:10 +0530

    Added validation for input dto

---
+++ src/main/java/com/shopping/rewards/dto/RewardRequest.java
@@ -0,0 +1,42 @@
+package com.shopping.rewards.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public class RewardRequest {
+
+	@NotNull
+	private String customerId;
+
+	@NotNull
+	@Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Date must be in yyyy-MM-dd format")
+	private String from;
+
+	@NotNull
+	@Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Date must be in yyyy-MM-dd format")
+	private String to;
+
+	public String getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(String id) {
+		this.customerId = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+}

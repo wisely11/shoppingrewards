1f82144035eabd3403f9f94c6e3ffdf66e398951
Wisely <wiselys@kriyatec.com>
2025-08-31 11:58:04 +0530

    Initial commit

---
+++ src/main/java/com/shopping/rewards/RewardsApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsApplication.class, args);
+	}
+
+}
